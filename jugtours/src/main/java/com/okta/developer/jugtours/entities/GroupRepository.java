d4c3f7078ce830e1375090ea0adb8bfbe308c067
Jason Tieu <jason.f.tieu@ou.edu>
2020-07-12 16:40:53 -0500

    sung made a change

--- jugtours/src/main/java/com/okta/developer/jugtours/entities/GroupRepository.java
+++ jugtours/src/main/java/com/okta/developer/jugtours/entities/GroupRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GroupRepository extends JpaRepository<Group, Long>
 {
+    // sung made a change here
     Group findByName(String name);
     // a change here
 }
+
